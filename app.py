c9db446c3cf879c08ebfb4f11dd41a0c00de257f
pozogo <ppozogo@gmail.com>
2022-01-22 16:29:53 +0000

    update ratelimit

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 limiter = Limiter(
     app,
     key_func=get_remote_address,
-    default_limits=["1 per second"]
+    default_limits=["100 per second"]
 )
 
 cors = CORS(app)
