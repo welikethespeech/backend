8afd98f5b1f854935fdbbce037950af9716c916a
pozogo <ppozogo@gmail.com>
2022-01-22 15:02:29 +0000

    implement backend and caching to backend

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-import db
+from db import Database
 from logger import logger
 import json
 import random
@@ -9,11 +9,39 @@ import flask
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+database = Database()
 
 @app.route("/api/score-speech", methods=["POST"])
 def api_score_speech():
     data = flask.request.json
+
+    # validation
+    if "company" not in data:
+        return jsonify({
+            "message": "company not provided"
+        }), 400
+    company = data["company"]
+
+    if "text" not in data:
+        return jsonify({
+            "message": "text not provided"
+        }), 400
+    text = data["text"]
+
     # do stuff with data[text]
-    return jsonify({
+
+    response_data = {
         "score": random.randint(0, 100)
-    })
+    }
+
+    # cache result
+    results_dict = database.data.get("results", {})
+    results_dict[company] = results_dict.get(company, []) + [response_data["score"]]
+    database.data["results"] = results_dict
+    database.save_to_file()
+
+    return jsonify(response_data)
+
+@app.route("/api/rankings", methods=["GET"])
+def api_rankings():
+    pass
--- db.py
+++ db.py
@@ -1,1 +1,14 @@
-# todo use json as db lmao
+import json
+
+class Database:
+    def __init__(self, path="data.json"):
+        self.path = path
+        self.data = {}
+
+    def load_from_file(self):
+        with open(self.path, "r") as f:
+            self.data = json.load(f)
+
+    def save_to_file(self):
+        with open(self.path, "w") as f:
+            json.dump(self.data, f)
